244c8beb45869764bca53577220b5ff8e67ee6ec
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-12-30 21:28:54 +0300

    add unity samples

---
+++ unity/urp/Assets/ThirdParty/NaninovelSpine/Runtime/SpineBackground.cs
@@ -0,0 +1,26 @@
+﻿namespace Naninovel
+{
+    /// <summary>
+    /// A <see cref="IBackgroundActor"/> implementation using <see cref="SpineController"/> to represent the actor.
+    /// </summary>
+    [ActorResources(typeof(SpineController), false)]
+    public class SpineBackground : SpineActor<BackgroundMetadata>, IBackgroundActor
+    {
+        private BackgroundMatcher matcher;
+
+        public SpineBackground (string id, BackgroundMetadata metadata)
+            : base(id, metadata) { }
+
+        public override async UniTask InitializeAsync ()
+        {
+            await base.InitializeAsync();
+            matcher = BackgroundMatcher.CreateFor(ActorMetadata, TransitionalRenderer);
+        }
+
+        public override void Dispose ()
+        {
+            base.Dispose();
+            matcher?.Stop();
+        }
+    }
+}
---
+++ unity/urp/Assets/ThirdParty/NaninovelSpine/Runtime/SpineCharacter.cs
@@ -0,0 +1,43 @@
+﻿using Naninovel.Commands;
+
+namespace Naninovel
+{
+    /// <summary>
+    /// A <see cref="ICharacterActor"/> implementation using <see cref="SpineController"/> to represent the actor.
+    /// </summary>
+    [ActorResources(typeof(SpineController), false)]
+    public class SpineCharacter : SpineActor<CharacterMetadata>, ICharacterActor, LipSync.IReceiver
+    {
+        public CharacterLookDirection LookDirection
+        {
+            get => TransitionalRenderer.GetLookDirection(ActorMetadata.BakedLookDirection);
+            set => TransitionalRenderer.SetLookDirection(value, ActorMetadata.BakedLookDirection);
+        }
+
+        protected virtual CharacterLipSyncer LipSyncer { get; private set; }
+
+        public SpineCharacter (string id, CharacterMetadata metadata)
+            : base(id, metadata) { }
+
+        public override async UniTask InitializeAsync ()
+        {
+            await base.InitializeAsync();
+            LipSyncer = new CharacterLipSyncer(Id, Controller.ChangeIsSpeaking);
+        }
+
+        public override void Dispose ()
+        {
+            LipSyncer.Dispose();
+            base.Dispose();
+        }
+
+        public UniTask ChangeLookDirectionAsync (CharacterLookDirection lookDirection, float duration,
+            EasingType easingType = default, AsyncToken asyncToken = default)
+        {
+            return TransitionalRenderer.ChangeLookDirectionAsync(lookDirection,
+                ActorMetadata.BakedLookDirection, duration, easingType, asyncToken);
+        }
+
+        public void AllowLipSync (bool active) => LipSyncer.SyncAllowed = active;
+    }
+}
